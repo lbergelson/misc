fa1005cf905d823be21291b5f73d96efdcc8ca7c
Louis Bergelson <louisb@broadinstitute.org>
2013-10-23 11:25:35 -0400

    Added cleanvcf.py script to clean periods from strelka 1.0.7 indel vcf files. --this is fixed in strelka v1.0.11

---
+++ MakeStrelkaPON/cleanvcf.py
@@ -0,0 +1,26 @@
+import sys
+
+#remove lines that have a "." as part of one of the alt variants.
+def cleanFile(file):
+    if not file.endswith(".vcf"):
+        print("{file} isn't a vcf".format(file=file))
+        return
+
+    f = open(file,'r')
+    clean = open(file.replace("vcf","clean.vcf") , 'w')
+    for line in f:
+        tokens = line.split("\t")
+        if len(tokens) >= 4:
+            if tokens[4].startswith('.') or tokens[4].endswith('.'):
+                return 
+        clean.write( "\t".join(tokens))
+    return 
+    
+def main(argv):
+    for arg in argv:
+        cleanFile(arg)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
+
+
